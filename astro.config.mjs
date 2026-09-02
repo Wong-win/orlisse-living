9fd879c31da1a0960155646c6e71ee2e21056dc2
Wong-win <luciferwangpf@gmail.com>
2026-09-02 16:33:53 +0300

    Build: 2026-09-02 16:33

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,2 @@
-import { defineConfig } from "astro/config";
-
-export default defineConfig({
-  site: "https://orlisseliving.com",
-});
+import { defineConfig } from "astro/config"; import sitemap from "@astrojs/sitemap";
+export default defineConfig({ site: "https://orlisseliving.com", integrations: [sitemap()], trailingSlash: "always" });
